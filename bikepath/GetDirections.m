3621a885faa90127d6414ff39a56c23f0a959c23
Armen Vartan & Molly Huerster <pair+armenvartan.mhuerster@devbootcamp.com>
2014-08-15 23:44:59 -0400

    function for getting directions from google maps

--- bikepath/GetDirections.m
+++ bikepath/GetDirections.m
@@ -7,7 +7,16 @@
 //
 
 #import "GetDirections.h"
+#import "AFNetworking.h"
 
 @implementation GetDirections
-
+    -(void) Directions {
+        AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+        [manager GET:@"https://maps.googleapis.com/maps/api/directions/json?mode=bycycling&origin=40.706038+-74.009070&destination=40.706638+-74.009070&z=12&key=AIzaSyDeifXgaBJFQSSUNQuC88lCq3M43tej5o4" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+            NSLog(@"JSON: %@", responseObject);
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+            NSLog(@"Error: %@", error);
+        }];
+        return;
+    };
 @end
